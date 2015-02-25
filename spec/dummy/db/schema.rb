377eaf40e47bdf06fc6ee7bd1d8e232e799b1f0c
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-02-25 12:46:44 +0100

    Add provider and uid fields to User [#85113770]

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215153026) do
+ActiveRecord::Schema.define(version: 20150224102948) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20141215153026) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.string   "uid"
+    t.string   "provider"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
