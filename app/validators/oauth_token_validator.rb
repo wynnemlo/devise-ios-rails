8b1718dd5602d17bce269782a5e1849eee4bf488
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-03-03 11:48:57 +0100

    Extract token validation logic [#85113772]

---
+++ app/validators/oauth_token_validator.rb
@@ -0,0 +1,23 @@
+class OauthTokenValidator < ActiveModel::Validator
+  def validate(record)
+    send "validate_#{record.provider}_token", record
+  end
+
+  private
+
+  def validate_facebook_token(record)
+    graph = Koala::Facebook::API.new record.oauth_token
+    graph.get_object 'me'
+  rescue Koala::Facebook::AuthenticationError => e
+    record.errors.add :oauth_token, e.fb_error_message
+  end
+
+  def validate_google_token(record)
+    conn = Faraday.new url: 'https://www.googleapis.com'
+    resp = conn.get "/oauth2/v1/tokeninfo?access_token=#{record.oauth_token}"
+    if resp.status == 400
+      error_description = JSON.parse(resp.body)['error_description']
+      record.errors.add :oauth_token, error_description
+    end
+  end
+end
--- lib/devise-ios-rails/oauth.rb
+++ lib/devise-ios-rails/oauth.rb
@@ -2,7 +2,7 @@ module DeviseIosRails
   module OAuth
     def self.included receiver
       receiver.extend ClassMethods
-      receiver.validate :oauth_token_valid, unless: 'provider.blank?'
+      receiver.validates_with OauthTokenValidator, unless: 'provider.blank?'
     end
 
     def email_required?
@@ -11,26 +11,6 @@ module DeviseIosRails
 
     def password_required?
       super && provider.blank?
-    end
-
-    def oauth_token_valid
-      send "validate_#{provider}_token"
-    end
-
-    def validate_facebook_token
-      graph = Koala::Facebook::API.new oauth_token
-      graph.get_object 'me'
-    rescue Koala::Facebook::AuthenticationError => e
-      errors.add :oauth_token, e.fb_error_message
-    end
-
-    def validate_google_token
-      conn = Faraday.new url: 'https://www.googleapis.com'
-      resp = conn.get "/oauth2/v1/tokeninfo?access_token=#{oauth_token}"
-      if resp.status == 400
-        error_description = JSON.parse(resp.body)['error_description']
-        errors.add :oauth_token, error_description
-      end
     end
 
     module ClassMethods
